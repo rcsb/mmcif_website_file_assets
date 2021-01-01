a17b800a65e8192aeb7137d4e2858491e05d4a56
jwest <john.westbrook@rcsb.org>
2020-12-31 20:02:28 -0500

    Add site reload script

--- scripts/RELOAD_SITE.sh
+++ scripts/RELOAD_SITE.sh
@@ -13,6 +13,9 @@ chown -R www-data:www-data /var/www/mmcif_website /var/www/mmcif_website_file_as
 echo "# Generating site content"
 /usr/local/bin/site_generator_cli --html   --web_gen_path /var/www/mmcif_website_generated --web_file_assets_path /var/www/mmcif_website_file_assets
 #
+echo "# Update search indices"
+/var/www/mmcif_website/mmcif_cgi/swish/MakeIndices.sh
+#
 echo "# Generating image content"
 /usr/local/bin/site_generator_cli --images --web_gen_path /var/www/mmcif_website_generated --web_file_assets_path /var/www/mmcif_website_file_assets
 #
